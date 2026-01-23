1048dd607c6626ee655d8b854a7aa112fd5c78e2
4drea4 <bakerand@outlook.com>
2026-01-23 10:12:41 -0600

    bookmark done going to do the route and then test

---
+++ models/Bookmark.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const bookmarkSchema = new mongoose.Schema(
+    {
+        title: {type:String, required: true},
+        url: {type: String},
+        note: { type: String},
+
+        user: {type:mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    },
+    {timestamps: true}
+);
