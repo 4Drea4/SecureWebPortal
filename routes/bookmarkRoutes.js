f8c1094d473f7b678e5b0381a7ca4a98b55817b5
4drea4 <bakerand@outlook.com>
2026-01-23 10:53:25 -0600

    create the bookmark

--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -5,3 +5,21 @@ const Bookmark = require('../models/Bookmark');
 const authMiddleware = require('../middleware/auth');
 
 //users create a bookmark and this way is protected by making it for the user thats logged in
+router.post ('/', authMiddleware, async (req, res) =>{
+    try {
+        const {title, url, note } = req.body;
+        if (!title) {
+            return res.status(400).json({message : 'Title is required'});
+        }
+        //from the jwt 
+        const newBookmark = await Bookmark.create({
+            title,
+            url,
+            note,
+            user:req.user._id, 
+        });
+        res.status(201).json(newBookmark);
+    } catch (error) {
+        res.status(400).json({message: "Uh Oh your bookmark could not be created", error : error.message})
+    }
+});
